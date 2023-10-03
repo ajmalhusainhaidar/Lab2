e91a6cef86f3bdd3f23a1eaf390237ae409d3409
ajmalhusainhaidar <143947072+ajmalhusainhaidar@users.noreply.github.com>
2023-10-03 10:47:41 -0700

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,33 +1,21 @@
-package com.greatlearnig.lab.transaction;
-import java.util.Scanner;
+package com.greatlearning.currency;
 
 public class Main {
 
 	public static void main(String[] args) {
-		Transaction transaction= new Transaction();
-		try (Scanner sc = new Scanner(System.in)) {
-			System.out.println(Integer.MAX_VALUE);
-			System.out.println("Enter the size of the transaction array");
-			int size =sc.nextInt();
-			int[] transactions =new int [size];
-			System.out.println("Enter the vaues of the transaction");
-			for (int i=0; i<size; i++) {
-				transactions[i] = sc.nextInt();
-			}
-			System.out.println("Enter the number of targets needs to be checked");
-			int targetSize = sc.nextInt();
-			for(int i=0; i<targetSize; i++) {
-				System.out.println("Enter the target value");
-			}
-			int target = sc.nextInt();
-			int res = transaction.targetCheck(transactions, target);
-			if( res >= 0) {
-				System.out.println("The target can be achieved after "+(int) (res+1) +" transactions");
-			}else {
-				System.out.println("The target cannot be achieved with given transaction");
-			}
+		int denominations[]= {60,5,12,78,25};
+		int payValue = 128;
+		MergeSortDesc sort = new MergeSortDesc();
+		sort.sort(denominations,0,denominations.length-1);
+		for(int val : denominations) {
+			System.out.println(val + " ");
 		}
+		System.out.println(" ");
+		Currency curr= new Currency();
+		curr.currencyCount(denominations,payValue);
 		
-	}
+		// TODO Auto-generated method stub
+
 	}
 
+}
